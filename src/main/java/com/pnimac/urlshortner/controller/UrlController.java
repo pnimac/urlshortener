79f007eb99a1cac1a9fe0326fda4a186a2728a75
“pnimac” <poornima.chauhan.2019@gmail.com>
2024-08-15 16:25:25 +1000

    Added URL Generation and Redirection services

--- src/main/java/com/pnimac/urlshortner/controller/UrlController.java
+++ src/main/java/com/pnimac/urlshortner/controller/UrlController.java
@@ -3,39 +3,44 @@ package com.pnimac.urlshortner.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.pnimac.urlshortner.config.AppConfig;
 import com.pnimac.urlshortner.data.UrlRequest;
 import com.pnimac.urlshortner.data.UrlResponse;
 import com.pnimac.urlshortner.model.UrlMapping;
-import com.pnimac.urlshortner.service.UrlService;
+import com.pnimac.urlshortner.service.UrlGenerationService;
+import com.pnimac.urlshortner.service.UrlRedirectionService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class UrlController {
 
-    private UrlService urlService;
+	private UrlGenerationService urlGenerationService;
+	private UrlRedirectionService urlRedirectionService;
 
-    private AppConfig appConfig;
+	@Autowired
+	public UrlController(UrlGenerationService urlGenerationService, UrlRedirectionService urlRedirectionService) {
+		this.urlGenerationService = urlGenerationService;
+		this.urlRedirectionService = urlRedirectionService;
+	}
 
-    @Autowired
-    public UrlController(UrlService urlService, AppConfig appConfig) {
-        this.urlService = urlService;
-        this.appConfig = appConfig;
-    }
-    
-    @PostMapping("/shorten")
-    public UrlResponse shortenUrl(@RequestBody UrlRequest request) {
-        UrlMapping urlMapping = urlService.createShortUrl(request.getLongUrl(), request.getCustomAlias(), request.getExpirationTime());
-        String shortUrl = urlMapping.getShortUrl();
-        return new UrlResponse(shortUrl);
-    }
+	@PostMapping("/shorten")
+	public UrlResponse shortenUrl(@RequestBody UrlRequest request) {
+		UrlMapping urlMapping = urlGenerationService.createShortUrl(request.getLongUrl(), request.getCustomAlias(),
+				request.getExpirationTime());
+		String shortUrl = urlMapping.getShortUrl();
+		return new UrlResponse(shortUrl);
+	}
 
-    @GetMapping("/{shortUrl}")
-    public void redirectUrl(@PathVariable String shortUrl, HttpServletResponse response) throws IOException {
-        String longUrl = urlService.getLongUrl(shortUrl);
-        response.sendRedirect(longUrl);
-    }
+	@GetMapping("/{shortUrl}")
+	public void redirectUrl(@PathVariable String shortUrl, HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String longUrl = urlRedirectionService.getLongUrl(shortUrl, request);
+		response.sendRedirect(longUrl);
+	}
 }
---
+++ src/main/java/com/pnimac/urlshortner/service/UrlRedirectionService.java
@@ -0,0 +1,38 @@
+package com.pnimac.urlshortner.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pnimac.urlshortner.model.UrlMapping;
+import com.pnimac.urlshortner.repository.UrlRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Service
+public class UrlRedirectionService {
+
+	@Autowired
+	private UrlRepository urlRepository;
+
+	@Autowired
+	private AnalyticsService analyticsService;
+
+	// Method to retrieve long URL for redirection
+	public String getLongUrl(String shortUrl, HttpServletRequest request) {
+		Optional<UrlMapping> urlMapping = urlRepository.findByShortUrl(shortUrl);
+		if (urlMapping.isPresent() && urlMapping.get().getExpirationDate() > System.currentTimeMillis()) {
+
+			// Track the click
+			String userAgent = request.getHeader("User-Agent");
+			String referrer = request.getHeader("Referer");
+			analyticsService.trackClick(shortUrl, userAgent, referrer);
+
+			return urlMapping.get().getLongUrl();
+		}
+		throw new RuntimeException("URL not found or expired");
+	}
+
+	
+}
